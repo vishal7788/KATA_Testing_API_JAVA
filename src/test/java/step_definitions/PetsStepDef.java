9e7e30f722e15735ef8abbf668921f0d7edf9288
Cristina <cristina.velican@sogeti.com>
2020-10-20 10:02:50 +0200

    changed class name

--- src/test/java/step_definitions/PetsStepDef.java
+++ src/test/java/step_definitions/PetsStepDef.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ExampleStepDef {
+public class PetsStepDef {
 
     private static ResponseBody body;
 
